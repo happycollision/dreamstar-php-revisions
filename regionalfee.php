087599cf5172e988a4ae874e866ff2e3fb32f131
Don Denton <don@happycollision.com>
2016-12-09 11:56:07 -0600

    Add regional fee page

---
+++ regionalfee.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<title>Regional Fee | Dreamstar Productions LLC.</title>
+
+<meta name="description" content="Regional Fee - Dreamstar Productions LLC. A  Family Oriented Dance Competition Company" />
+<meta name="keywords" content="Dance Competition, Family Oriented, Dancing Competitions,Dreamstar Dance Competition Regional Fees, Dance Dreamstar" />
+
+
+
+<?php
+# Load Layout
+require_once("V2/_CSS_Files/head.php");
+$Title = "Regional Entry Fee";
+
+?>
+<div class="containter">
+<div class="header"><?php require_once("V2/_CSS_Files/menu.php"); ?></div><!-- End header -->
+<div class="content"><p class="PageTitle"><?php echo $Title; ?></p> 
+<ul>
+	<li><strong>Solo</strong>: $75</li>
+    <li><strong>Duet/Trio</strong>: $85        </li>
+    <li><strong> Solo &amp; Duet/Trio extended time  (up to 4:30)</strong>: $2 addl. per routine <br /></li>
+    <li><strong>Small Group, Large Group, Line</strong>: $30  per person<br /></li>
+    <li><strong>Production (Lines Only)</strong>: $35 per person</li>
+    <li><strong>SmallGroup, Large Group, Line Extended Time</strong>: $2  addl. per person</li>
+    <li><strong>Small Group, Large Group, Line,  Production Expanded Time</strong>: $3 addl. per person </li>
+    <li><strong>Entry Late Fee</strong>: $10 per entry</li>
+</ul>
+<p><strong>Note - Absolutely  no cash refunds after deadline date!</strong></p>
+
+</div><!-- End content  -->
+<div class="tourdates"><?php require_once("V2/_CSS_Files/dates.php");?> </div> <!-- End tourdates -->
+<div class="footer"><?php require_once("V2/_CSS_Files/footer.php");?> </div> <!-- End footer -->
+</div> <!-- Containter End -->
+</body>
+</html>
+
