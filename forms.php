92f6e07f93aa720eca7d4ce0da15b986359a3f03
Don Denton <don@happycollision.com>
2016-12-11 12:27:27 -0600

    Add forms.php to modify

---
+++ forms.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<title>Official Forms - Dreamstar Production LLC.</title>
+
+<meta name="description" content="Offical Forms - Dreamstar Productions LLC. A  Family Oriented Dance Competition Company" />
+<meta name="keywords" content="Dance Competition, Family Oriented, Dancing Competitions,Dreamstar Dance Compeitition Offical Forms, Dance Dreamstar" />
+
+
+
+<?php
+# Load Layout
+require_once("V2/_CSS_Files/head.php");
+$Title = "Entry Forms";
+
+?>
+
+<div class="containter">
+<div class="header"><?php require_once("V2/_CSS_Files/menu.php"); ?></div><!-- End header -->
+<div class="content"><p class="PageTitle"><?php echo $Title; ?></p>
+
+
+<h2>Please Choose a form below:</h2>
+
+<h3>Note: Some pages have special instructions. </h3>
+
+<p><a href="entryforms.php?Form=Entry" class="dreamstar-links">Click Here to Download Entry Form (pdf)</a></p>
+<p><a href="entryforms.php?Form=Waiver" class="dreamstar-links">Click Here to Download Waiver and Release Form (pdf)</a></p>
+<p><a href="entryforms.php?Form=Master" class="dreamstar-links">Click Here to Download Master Class Registration Form (pdf)</a></p>
+
+</div><!-- End content  -->
+<div class="tourdates"><?php require_once("V2/_CSS_Files/dates.php");?> </div> <!-- End tourdates -->
+<div class="footer"><?php require_once("V2/_CSS_Files/footer.php");?> </div> <!-- End footer -->
+</div> <!-- Containter End -->
+</body>
+</html>
