4e1ce33dfed7193285010834bf79ee59c3421497
Don Denton <don@happycollision.com>
2016-12-20 07:26:10 -0600

    add judges page to alter bios

---
+++ judges.php
@@ -0,0 +1,170 @@
+<?php
+# Judge Profiles
+function MattPospeshil($masterclass){ ?>
+<div class="Bio-Container">
+	<div class="Bio-Mug"><img class="Bio-Mug" src="/_JPG_Files/headshots/web-headshots/Matt.Pospeshil.jpg" alt="Judge Headshot"/></div>
+	<div class="Bio-Name">Matt Pospeshil</div>
+	<div class="Bio-Title">Competition Judge <?php if($masterclass == "True"){ echo "/ Master Class Instructor";} ?></div>
+	<div class="Bio-Info"><p>Matt Pospeshil grew up dancing, singing & performing theater in the northern suburbs of Chicago, and got his start professionally with Especially Tap Chicago (dir. Julie Cartier). He furthered his education at Western Michigan University and got a BA in Dance. After college, Matt returned home to dance in Chicago. He danced with Inaside Chicago Dance (dir. Richard Smith), & began to manage his family’s dance studio in Buffalo Grove, IL ­ Kristina’s Studio of Dance. Post dancing with Inaside he returned to tap dance professionally. Matt had the pleasure to work with Chicago Tap Theatre (dir. Mark Yonally) and The Cartier Collective. Matt's convention teaching credits include Dance Masters of America ­ Great Lakes Regional, Louisiana & Dance Masters of Wisconsin. He currently is dancing with Chicago Human Rhythm Project's resident company, BAM! (dir. Lane Alexander). He directs the youth dance company, the Heart of Dance Ensemble, based out of Kristina's Studio of Dance. Matt’s professional directorial & production debut was Fuse & Explode ­ Concert of Song & Dance. This concert, like his establishing company ­ Troubadour Dance Theatre, featured his choreography in multiple styles of dance. </p>
+    </div>
+</div>
+<?php }
+function EricaMcCool($masterclass){ ?>
+<div class="Bio-Container">
+	<div class="Bio-Mug"><img class="Bio-Mug" src="/_JPG_Files/headshots/web-headshots/Erica.McCool.jpg" alt="Judge Headshot"/></div>
+	<div class="Bio-Name">Erica McCool</div>
+	<div class="Bio-Title">Competition Judge <?php if($masterclass == "True"){ echo "/ Master Class Instructor";} ?></div>
+	<div class="Bio-Info"><p>Erica McCool began her dance training at the Central Wisconsin School of Ballet under the direction Waltraud G. Karkar. There she studied Vaganova Method ballet with such greats as Irina Kolpakova and Vladilen Semenov of the Kirov, classical jazz and tap with dance legends Luigi and Judy Ann Bassing, and Isadora Duncan technique with Jeanne Bresciani. After completing a Bachelor of Science in Modern Dance Performance from the University of Wisconsin-Madison, Erica spent the next 11 years instructing dancers of all ages and choreographing for the stage. In 2004, she was invited to perform for a sold out crowd of over 2,000 people at the Grand Opening of the Overture Center, Madison's premiere performing arts venue. Whether through teaching, choreographing, or judging, Erica hopes to enrich the lives of young dancers by sharing her own passion for the performing arts.
+</p>
+    </div>
+</div>
+<?php }
+function JacquelineSpreadbury($masterclass){ ?>
+<div class="Bio-Container">
+	<div class="Bio-Mug"><img class="Bio-Mug" src="/_JPG_Files/headshots/web-headshots/Jacqueline.Spreadbury.jpg" alt="Judge Headshot"/></div>
+	<div class="Bio-Name">Jacqueline Spreadbury</div>
+	<div class="Bio-Title">Competition Judge <?php if($masterclass == "True"){ echo "/ Master Class Instructor";} ?></div>
+	<div class="Bio-Info"><p>Jacqueline Spreadbury is  from Chicago, IL and has been dancing for 23 years, training extensively in tap, jazz, and ballet, under Jay and Connie Fagan, Tre Dumas, Lane Alexander, Mark Yonally, Bril Barrett, and Sue Gramza.  Recently, she has been featured as a lead dancer in Fuse and Explode (directed by Matt Pospeshil).  She had the honor of performing with Chicago Tap Theater in their production Love Taps in March 2014, and has performed each year in the Chicago Thanksgiving Day Parade on WGN. </p><p>She is the artistic director of Legacy Dance Ensemble in Midlothian IL, and her students were awarded the title of 2015 Grand National Champions by Midwest Starz Dance Comeptition.  Jacqueline is also an award-winning choreographer; she was nominated for Best Tap Dance of 2014 by the Industry Dance Awards for her piece, “Just A Girl”.  Her choreography has been the recipient of many awards, including “Best of Choreography” awards at national competitions five years in a row and has received “Diamond Level” choreography awards as recently as 2015.  She has been the recipient of the Leo Harris scholarship for excellence in tap.  As a youth, Jacqueline had the opportunity to compete on various competition teams, taking home many top awards, including “Impeccable Tap Technique”.  Jacqueline's choreography has been featured in local musicals including Annie, The Music Man, and Beauty & The Beast. Jacqueline was a four-year member of the University of Chicago's UC Dancers and was featured in the University of Chicago’s presentation of City Life, as both a performer and choreographer. As a performing member of Rhythmic Bodies in Motion, Jacqueline has been featured in shows, including Body Language and Revel in the Rhythm. Jacqueline has a masters degree in chemistry from Governors State University.</p>
+    </div>
+</div>
+<?php }
+function KimMunn($masterclass){ ?>
+<div class="Bio-Container">
+	<div class="Bio-Mug"><img class="Bio-Mug" src="/_JPG_Files/headshots/web-headshots/KimMunn.JPG" alt="Judge Headshot"/></div>
+	<div class="Bio-Name">Kim Munn</div>
+	<div class="Bio-Title">Competition Judge <?php if($masterclass == "True"){ echo "/ Master Class Instructor";} ?></div>
+	<div class="Bio-Info"><p>Kim has been dancing all of her life. She began dancing at the age of two, competing at the age of seven, and has won ten Grand National championships over her career along with countless titles and special awards. Kimberly was also featured in the October 2002 issue of Dance Spirit Magazine as one of the 50 national future stars.</p>
+    <p>To further her dancing career, Kim has attended Dance Masters of America Teacher Training School and has received her graduate diploma of dance education in tap, jazz and ballet. Kim owns and operates one of the largest studios in Iowa that is home to a nationally ranked competitive team. Her choreography and students have won numerous awards throughout the country. She's also a proud founding member of GIFT of the Arts foundation which provides the arts to those less fortunate and the National award winning all star dance team Powerhouse Elite. She resides in Bettendorf, Iowa with her husband Rob, and their two children Sam and Maicey.</p>
+    </div>
+</div>
+<?php }
+function EvelynRiceWells($masterclass){ ?>
+<div class="Bio-Container">
+	<div class="Bio-Mug"><img class="Bio-Mug" src="/_JPG_Files/headshots/web-headshots/EvelynRiceWells.jpg" alt="Judge Headshot"/></div>
+	<div class="Bio-Name">Evelyn Rice</div>
+	<div class="Bio-Title">Competition Judge <?php if($masterclass == "True"){ echo "/ Master Class Instructor";} ?></div>
+	<div class="Bio-Info"><p>Evelyn Rice is the founder and co-director of The Exposure Movement, Inc., an artist development company that exposes and educates young artists through 
+media and performing arts. She has been working on-camera and behind-the-scenes in the entertainment industry for many years. </p>
+	  <p>Evelyn has been fortunate to learn and grow through many arrangements of multimedia events. From choreographing routines for College and High School dance teams, to modeling, performing and directing Arts & Entertainment projects nationwide, venturing onto dance stages with recording artists including, Kimberly Locke, Jennifer Holiday, Alfonso Hunter, Twista, and native Chicago artists, such as K.W.O.E., Drew Sidora, Endiskize, Javante Monet, and Kim Keller. </p>
+<p>Her passion is constantly being fed from the talented people she has the honor of training with and working alongside. Evelyn recently collaborated with OSIP and Agency EA to direct an extravagant corporate stage production. She worked with Stage Factor and KMS on the flash mob for America’s Beauty Show 2012. As a freelance choreographer, she works with many studios, high schools, and colleges all over the country, which compete at Regional and National conventions/competitions. Evelyn is a hip hop educator for the Chicago National Association of Dance Masters (CNADM). </p>
+<p>Other recent credits include: WGN-TV, Dreamstar Productions 2013-15, The Carnival 2012-14, Pivot Point International 2010, 2008 Future Vision Tour, Vicious, Dance 2XS, UDA, Varsity.com, Cheer and Dance Association (CDA), America’s Beauty Show, The Chicago Confidential, Glamour Magazine April 2007 issue, NIKE Rockstar Workout Winner 2006, and she was also featured in Atlantic recording artist, Bayje’s music video, “Jealous of Your Whip.” Evelyn is currently working on upcoming choreography for performing artists and dance-driven projects, while also coaching at Soul2Sole Dance Studio and teaching at Visceral Dance Center.</p>
+    </div>
+</div>
+<?php }
+function SamKivi(){ ?>
+<div class="Bio-Container">
+	<div class="Bio-Mug"><img class="Bio-Mug" src="/_JPG_Files/headshots/web-headshots/Sam-Kivi.jpg" alt="Judge Headshot"/></div>
+	<div class="Bio-Name">Sam Kivi</div>
+	<div class="Bio-Title">Competition Judge</div>
+	<div class="Bio-Info"><p>Samuel Kivi is thrilled to be siting on the Dreamstar judging panel this year for the second time.  </p>
+	  <p>Originally from MN, Sam has been actively coaching young people in theatre song and dance for over 20 years, appearing as a clinician at numerous national level showcases and competitions. Sam earned his BA degree in vocal performance and choreography from Luther College, in Decorah, IA. and has studied dance with  Jon Command, Michael Farrell, Janet Gillespie, and Doug Risner and taken master classes with <em>Dance Spectrum, Zenon Dance Company and School/Lar Lubovitch Dance Company, Les Ballets Africans, and the Bella Lewitzky Dance Company.</em>     </p>
+	  <p>As a performer Sam played the lovable Joey Vitale and served as Choreographer/Dance Captain for 5 years in the National Phenomenon, <em>TONY N&rsquo; TINA&rsquo;S WEDDING</em>.  He also appeared as Bernardo in <em>WEST SIDE STORY</em>, Sparky in<em>FOREVER PLAID </em>and in Bill Russell and Henry Krieger&rsquo;s <em>SIDE SHOW</em>, directed by Bill Russell.  Additionally, Sam appeared in the LA Premiere of <em>PAGEANT</em> and Peter Schneider&rsquo;s <em>GRAND HOTEL</em> at The Colony Theatre.  Other favorites include <em>THE RINK</em>, <em>SONGS FOR A NEW WORLD</em>, 2 Nat&rsquo;l tours of <em>BABES IN TOYLAND</em> as dance captain, <em>JOSEPH and the AMAZING TECHNICOLOR DREAMCOAT</em>, and <em>GOOD NEWS</em> with actress Amy Adams.  He has also appeared in a variety of National commercials and independent films.</p>
+	  <p>For the past 11 years SAM has been living in Los Angels working at the helm of his casting company KIVI CASTING.  He has worked on numerous projects for networks such as; NBC, ABC, CBS, BRAVO, DISCOVERY, A&amp;E, MTV, and TLC.  His most recent projects include, INA's <em>CRAZY SEXY WILD</em> musicvideo and  <em>REAL LIFE THE MUSICAL</em> for OWN TV where he had the distinct pleasure of teaming up with the creative team from the hit-show, GLEE. He also had the opportunity to interview and work with top broadway choreographers Josh Prince, Justin Greer, Jamal Sims from the <em>STEP UP AND DANCE</em> movie franchise, and TV Choreographers Tony Gonzalez and Stacey Tookey from <em>SO YOU THINK YOU CAN DANCE. </em></p>
+	  <p>Currently Samuel works in Minneapolis MN for Broadway Across America and for The Producing House.</p>
+	  <p>Samuel couldn't be prouder to be a part of the Dreamstar team.</p>
+    </div>
+</div>
+<?php }
+function JenniferGonzalez($masterclass){ ?>
+<div class="Bio-Container">
+	<div class="Bio-Mug"><img class="Bio-Mug" src="/_JPG_Files/headshots/web-headshots/JenniferG.jpg" alt="Judge Headshot"/></div>
+	<div class="Bio-Name">Jennifer Haws</div>
+	<div class="Bio-Title">Competition Judge <?php if($masterclass == "True"){ echo "/ Master Class Instructor";} ?></div>
+	<div class="Bio-Info">
+	  <p>Jennifer Haws grew up in Southern California and there she became a passionate
+
+and competitive dancer. She obtained a Bachelor of Fine Arts in Dance from the 
+
+University of Colorado in Boulder where she was a three-time recipient of the 
+
+University Dance Award. Jennifer has been a teacher and choreographer for over 20 
+
+years in Southern California, Colorado, and New York.</p><p>
+
+Jennifer’s dance education includes Jazz, Modern, Hip-hop, Tap Ballet Mexican folk, 
+
+Tahitian, and African including scholarships to Perry-Mansfield Performing Arts 
+
+School and Cleo Parker Robinson Dance School.</p><p>
+
+She has performed in and choreographed independent musicals and shows, 
+
+in addition to producing 2 dance productions in New York City and Boulder, 
+
+CO. Jennifer has performed with the Damsels Dance Ensemble, Wide Awake 
+
+Dance Company, and CU Moving Company in Colorado. She currently runs the 
+
+Jazz program at Littleton Dance Academy and choreographs for the theatre and 
+
+choir departments at Alameda High School. Jennifer’s vitality and enthusiasm is 
+
+contagious!</p>
+	</div>
+</div>
+<?php }
+function ColleenKosier (){ ?>
+<div class="Bio-Container">
+	<div class="Bio-Mug"><img class="Bio-Mug" src="/_JPG_Files/headshots/web-headshots/Colleen-Kosier.jpg" alt="Judge Headshot"/></div>
+	<div class="Bio-Name">Colleen Kosier</div>
+	<div class="Bio-Title">Competition Judge</div>
+	<div class="Bio-Info"><p>Colleen Kosier began her dance and tumbling education at the age of 5.  She continued with a
+
+double major in Dance and Physical Education at Illinois State University where she enjoyed 
+
+traveling, performing, and competing with the Illinois State Dance Theatre.  </p><p>She opened her 
+
+own studio in 1979 and has enjoyed teaching dance and tumbling for over 30 years.  Currently 
+
+Colleen is the owner and director of two dance schools in Illinois.  She also teaches classes at 
+
+JDC and at National Workshops.  She is the Director and Choreographer for the Celtic Rhythm 
+
+Dancers and the American Rhythm Cloggers.  </p><p>Her dancers and choreography have won top 
+
+honors in competition, have performed at half-time shows for college and semi-pro basketball 
+
+games, opened minor league baseball games, performed at county fairs, state fairs, Celtic 
+
+Festivals around the U.S. and five times at Walt Disney World.  Colleen and her dancers toured 
+
+and performed with the renowned Celtic Band “Switchback” across the beautiful country of 
+
+Ireland and has headlined with the Canadian Band “Searson”.  </p><p>Colleen wishes all today’s 
+
+dancers a great performance and a life time of enjoying the beautiful art of dance.</p>
+	</div>
+</div>
+<?php }
+
+function KimHale ($masterclass){ ?>
+<div class="Bio-Container">
+	<div class="Bio-Mug"><img class="Bio-Mug" src="/_JPG_Files/headshots/web-headshots/Kim-hale.jpg" alt="Judge Headshot"/></div>
+	<div class="Bio-Name">Kim Hale</div>
+	<div class="Bio-Title">Competition Judge <?php if($masterclass == "True"){ echo "/ Master Class Instructor";} ?> / Adjudicator</div>
+	<div class="Bio-Info"><p>Kim Hale is honored to join the Dance Education team at The Movement Talent Agency bringing with her a wealth of experience as a teacher, choreographer, performer and advocate for Arts Education. A magna cum laude graduate of Wellesley College, Kim Hale is a highly respected teacher of jazz and theater dance on both coasts known for her ability to train technically strong, passionate and “working” professionals. </p>
+	  <p>Kim spent five years as Assistant Professor of Dance in the BFA Musical Theater program at Syracuse University where she taught all levels of jazz, theater dance and ballet. She is currently on the dance faculty at AMDA-LA, Los Angeles County High School for the Arts, Debbie Allen Dance Academy as well as a guest teacher at The Edge in Los Angeles and Steps on Broadway in New York City. </p>
+	  <p>Kim has been active on the dance competition and convention circuit for the past ten years. Her former students have performed in all facets of the entertainment industry including Broadway productions of Kinky Boots, White Christmas, Billy Elliot, Ghost the Musical, Mamma Mia, American Idiot, Wicked, Rent, A Chorus Line and West Side Story; numerous National Tours; films such as Hairspray and Step Up 3; national commercials and as backup dancers for Beyoncé and Mariah Carey.</p>
+	  <p>Visit her website @ <a href="www.kimhaledance.com" target="_blank">kimhaledance.com</a> , come to a Dreamstar Dance Competition and sign up for Kim’s Master Class.</p>
+    </div>
+</div>
+<?php }
+
+function SaraBartlett ($masterclass){ ?>
+<div class="Bio-Container">
+	<div class="Bio-Mug"><img class="Bio-Mug" src="/_JPG_Files/headshots/web-headshots/Sara-Bartlett.jpg" alt="Judge Headshot"/></div>
+	<div class="Bio-Name">Sara Bartlett</div>
+	<div class="Bio-Title">Competition Judge <?php if($masterclass == "True"){ echo "/ Master Class Instructor";} ?></div>
+	<div class="Bio-Info"><p>Sara received a degree in Dance from Northwestern University and danced professionally as a Radio City Rockette. She has been choreographing for schools and community theaters around the Madison area for the past fifteen years and has over 25 musicals under her belt. </p><p>Sara also taught Advanced Tap and Jazz classes from 2001-2012 and choreographed award-winning routines for her dancers' regional competitions. She has been seen on stage locally with Madison Ballet, Madison Opera, Four Seasons Theatre, and Middleton Players Theater.</p></div>
+</div>
+<?php }
+
+/*
+<div class="Bio-Container">
+<div class="Bio-Mug"><img class="Bio-Mug" src="#" alt="Judge Headshot"/></div>
+<div class="Bio-Name"></div>
+<div class="Bio-Title"></div>
+<div class="Bio-Info"></div>
+</div>
+
+*/
